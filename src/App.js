f80c295a622bc4cc820d177c1f06c53bef29550a
alexis39tt <alessandro.fonzi@gmail.com>
2022-09-20 17:19:29 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("anonymous")
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
